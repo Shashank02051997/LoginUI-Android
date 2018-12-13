f1624e7498a64f94c4593e4cba5cd8e70d876873
Juan Ramos <juancagb.17@gmail.com>
2018-12-12 21:38:55 -0500

    Refactor change background logic, add private visibility and remove unused imports

--- app/src/main/java/com/shashank/platform/loginui/MainActivity.java
+++ app/src/main/java/com/shashank/platform/loginui/MainActivity.java
@@ -1,19 +1,20 @@
 package com.shashank.platform.loginui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView imageView;
-    TextView textView;
-    int count = 0;
+    private ImageView imageView;
+    private TextView textView;
+    private boolean morning = true;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,37 +23,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
-        imageView.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
-            public void onSwipeTop() {
-            }
 
+        // you should avoid using the following behavior (it is only for demo purposes)
+        imageView.setOnTouchListener(new OnSwipeTouchListener(getApplicationContext()) {
             public void onSwipeRight() {
-                if (count == 0) {
-                    imageView.setImageResource(R.drawable.good_night_img);
-                    textView.setText("Night");
-                    count = 1;
-                } else {
-                    imageView.setImageResource(R.drawable.good_morning_img);
-                    textView.setText("Morning");
-                    count = 0;
-                }
+                changeBackground();
             }
 
             public void onSwipeLeft() {
-                if (count == 0) {
-                    imageView.setImageResource(R.drawable.good_night_img);
-                    textView.setText("Night");
-                    count = 1;
-                } else {
-                    imageView.setImageResource(R.drawable.good_morning_img);
-                    textView.setText("Morning");
-                    count = 0;
-                }
+                changeBackground();
             }
-
-            public void onSwipeBottom() {
-            }
-
         });
+    }
+
+    private void changeBackground() {
+        if (morning) {
+            imageView.setImageResource(R.drawable.good_night_img);
+            textView.setText("Night");
+            morning = false;
+        } else {
+            imageView.setImageResource(R.drawable.good_morning_img);
+            textView.setText("Morning");
+            morning = true;
+        }
     }
 }
